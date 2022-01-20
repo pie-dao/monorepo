ba3055bacae988db9ae480cba40dcb916ba28df9
Totò Volturno <chiptuttofuso@gmail.com>
2022-01-20 12:38:27 +0100

    fixed class error

--- apps/landing-page/components/Metaverse.js
+++ apps/landing-page/components/Metaverse.js
@@ -6,7 +6,7 @@ const Metaverse = ({}) => {
     <section classNameName={`bg-primary w-full justify-evenly content-center text-center flex flex-row md:flex-col`}>
       <div className="flex mb-4 ml-12 mr-12">
         <div className="w-1/2 p-10 pt-[5pc]">
-          <h1 className="text-4xl text-highlight uppercase">What <span class="font-bold">is the metaverse?</span></h1>
+          <h1 className="text-4xl text-highlight uppercase">What <span className="font-bold">is the metaverse?</span></h1>
           <h2>{content.metaverse.description}</h2>
           <div className="flex mb-4 mt-4">
             <div className="grid content-around rounded-xl w-1/2 border-4 border-transparent border-l-highlight pl-4">
@@ -21,11 +21,11 @@ const Metaverse = ({}) => {
           <img src="/metaverse_man.svg" />          
         </div>
       </div>
-      <div class="flex mb-4">
-        <div class="w-1/4 bg-gray-500 h-12"></div>
-        <div class="w-1/4 bg-gray-400 h-12"></div>
-        <div class="w-1/4 bg-gray-500 h-12"></div>
-        <div class="w-1/4 bg-gray-400 h-12"></div>
+      <div className="flex mb-4">
+        <div className="w-1/4 bg-gray-500 h-12"></div>
+        <div className="w-1/4 bg-gray-400 h-12"></div>
+        <div className="w-1/4 bg-gray-500 h-12"></div>
+        <div className="w-1/4 bg-gray-400 h-12"></div>
       </div>      
     </section>
   );
