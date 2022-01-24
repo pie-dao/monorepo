bb6e79265b214592dfae0bf0d39dbad138f3a0db
Totò Volturno <chiptuttofuso@gmail.com>
2022-01-24 11:11:46 +0100

    completed scrolling effect

--- apps/landing-page/components/ScrollingBoxes.js
+++ apps/landing-page/components/ScrollingBoxes.js
@@ -4,15 +4,15 @@ import content from "../content/en_EN.json";
 const ScrollingBoxes = ({}) => {
   return (
     <section className={`w-full justify-evenly content-left text-left m-10`}>
-      <div className="bg-secondary p-12 md:pl-24 md:pr-24 h-60">
+      <div className="z-40 sticky top-0 bg-secondary p-12 md:pl-24 md:pr-24 h-60">
         <p className="uppercase text-4xl">Easy access to<br />
           <span className="font-bold">the metaverse</span>
         </p>
         <p className="hidden md:block w-1/3 pt-4">{content.scrolling_boxes.description}</p>            
       </div>
-      <div className="flex mb-4 relative">
+      <div className="z-50 sticky flex mb-4">
         <div className="hidden md:block w-1/2"></div>
-        <div className="w-full md:w-1/2 m-8 absolute right-0 -top-52 overflow-hidden">
+        <div className="w-full md:w-1/2 m-8 right-0 -top-52">
           {content.scrolling_boxes.boxes.map(box => {
             return(
               <div key={box.id} className="p-1 rounded-md bg-gradient-to-tr from-red-500 to-blue-500 mb-4">
