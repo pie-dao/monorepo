3f47604ad4c00f7fe64515530b97555954a8c553
knx <gabrielebonumore@gmail.com>
2022-01-29 16:09:15 +0100

    Add Color and css vars for swiper

--- apps/landing-page/tailwind.config.js
+++ apps/landing-page/tailwind.config.js
@@ -1,23 +1,21 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: 'jit',
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",      
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",    
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
     extend: {
       colors: {
-        primary: '#150110',
-        secondary: '#412D87',
-        highlight: '#D7099C',
-        highlight_secondary: '#32C7FE'
+        primary: "#150110",
+        secondary: "#412D87",
+        highlight: "#D7099C",
+        highlight_secondary: "#32C7FE",
+        deep_blue: "#367BF5",
       },
     },
   },
