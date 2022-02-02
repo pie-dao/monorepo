443160248a4d99e0d676a4f7bbd77f97d2921e3d
jordaniza <jordan.imran@predixa.ai>
2022-02-02 19:22:59 +0400

    added ftm to connectors

--- apps/mono-app/src/utils/networks.ts
+++ apps/mono-app/src/utils/networks.ts
@@ -1,57 +1,35 @@
-export enum ChainIds {
-  LOCAL = 31337,
-  POLYGON = 137,
-  MAINNET = 1,
-}
-
-export const SupportedChains: Record<string, number> = {
-  LOCAL: 31337,
-  POLYGON: 137,
-  MAINNET: 1,
-};
-
-export type SUPPORTEDNETWORKS = "POLYGON" | "LOCAL" | "AVALANCHE";
+export type SUPPORTEDNETWORKS = 'POLYGON' | 'LOCAL' | 'FANTOM' | 'ETHEREUM';
 
 export type NetworkDetails = {
-  name: string;
+  name: SUPPORTEDNETWORKS;
   chainId: number;
-};
+}
 
 export type Networks = {
-  [N in SUPPORTEDNETWORKS]: NetworkDetails;
-};
+  [N in SUPPORTEDNETWORKS]: NetworkDetails
+}
 
 export type _NetworkDetail = {
-  name: string;
+  name:  SUPPORTEDNETWORKS;
   symbol: string;
   color: string;
-};
-export type ChainMap = Record<number, _NetworkDetail>;
+}
+export type ChainMap = Record<number, _NetworkDetail>
 
 export const chainMap: ChainMap = {
   137: {
-    name: "Polygon",
-    color: "purple-700",
-    symbol: "MATIC",
+    name: 'POLYGON',
+    color: 'purple-700',
+    symbol: 'MATIC'
   },
   1: {
-    name: "Ethereum",
-    color: "blue-400",
-    symbol: "ETH",
+    name: 'ETHEREUM',
+    color: 'blue-400',
+    symbol: 'ETH'
   },
-};
-
-export const NETWORKS: Networks = {
-  POLYGON: {
-    name: "Polygon",
-    chainId: ChainIds.POLYGON,
-  },
-  LOCAL: {
-    name: "localhost",
-    chainId: ChainIds.LOCAL,
-  },
-  AVALANCHE: {
-    name: "Avalanche",
-    chainId: ChainIds.POLYGON,
-  },
-};
+  250: {
+    name: 'FANTOM',
+    color: 'blue-700',
+    symbol: 'FTM'
+  }
+}
