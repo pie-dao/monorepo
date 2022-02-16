97c157af7ef75f50209690401a2682ce37030e0d
knx <gabrielebonumore@gmail.com>
2022-02-16 17:32:46 +0100

    update gtag

--- apps/landing-page/utils/GTM/operations/share.js
+++ apps/landing-page/utils/GTM/operations/share.js
@@ -1,8 +1,7 @@
 import GTM from "../constants";
 
 const pushShareData = (socialName, id) => {
-  gtag({
-    event: GTM.EVENT.SHARE,
+  gtag("event", GTM.EVENT.SHARE, {
     method: socialName,
     content_type: "url",
     item_id: id,
