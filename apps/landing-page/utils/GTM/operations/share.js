93bb109e4c8b75b8a24e410246406f0d2caa3dad
knx <gabrielebonumore@gmail.com>
2022-02-16 17:26:36 +0100

    remove consolelog on gtm

--- apps/landing-page/utils/GTM/operations/share.js
+++ apps/landing-page/utils/GTM/operations/share.js
@@ -1,12 +1,6 @@
 import GTM from "../constants";
 
 const pushShareData = (socialName, id) => {
-  console.log({
-    event: GTM.EVENT.SHARE,
-    method: socialName,
-    content_type: "url",
-    item_id: id,
-  });
   gtag({
     event: GTM.EVENT.SHARE,
     method: socialName,
