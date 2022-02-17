ab3b81c6f96259d88f71b8847dd9ee23ba7dba28
Alessio Delmonti <alessio.d@gmail.com>
2022-02-17 15:07:32 +0000

    Adding vaults

--- apps/auxo-app/src/store/vault/vault.state.ts
+++ apps/auxo-app/src/store/vault/vault.state.ts
@@ -85,6 +85,93 @@ export const vaultState: VaultState = {
         }
       ]
     },   
+    {
+      id: 2,
+      name: 'FRAX FTM',
+      description: `
+      Frax is a stable coin pegged to U.S. dollar value issued by Frax Finance.
+      Auxo FRAX Vault seeks yield by allocating deposited FRAX on strategies involving lending, liquidity provision, yield farming and more.
+      `,
+      network: {
+        name: 'FANTOM',
+        chainId: 250
+      },
+      symbol: 'FRAX',
+      address: '0xBC4639e6056c299b5A957C213bcE3ea47210e2BD',
+      token: {
+        decimals: 18,
+        address: '0xdc301622e621166bd8e82f2ca0a26c13ad0be355'
+      },
+      auth: {
+        address: '0xA86fc7aD871B5247f13BB38a08a67bE4d38e577B',
+        isDepositor: false,
+      },
+      cap: {
+        address: '0xBC4639e6056c299b5A957C213bcE3ea47210e2BD',
+        underlying: null
+      },
+      strategies: [
+        {
+          name: 'Tarot Lender',
+          allocation: 1,
+          description: `
+          Supplies FRAX on Tarot decentralized lending protocol. 
+          Tarot gives users the opportunity to partecipate as lenders in isolated lending pools. 
+          
+          The strategy rebalances the available FRAX between Tarot isolated lending pools to have the best yield possible.
+          `,
+          links: [
+            {
+              name: 'Tarot',
+              to: 'https://www.tarot.to'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'wFTM FTM',
+      description: `
+      Fantom is a smart contract platform that intends to solve the scalability issues of existing public distributed ledger technologies. FTM is the native token of the Fantom ecosystem and wFTM is its wrapped version.
+      You can wrap your FTM for wFTM at Sushi.
+      `,
+      network: {
+        name: 'FANTOM',
+        chainId: 250
+      },
+      symbol: 'wFTM',
+      address: '0x16AD251B49E62995eC6f1b6A8F48A7004666397C',
+      token: {
+        decimals: 18,
+        address: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83'
+      },
+      auth: {
+        address: '0xA86fc7aD871B5247f13BB38a08a67bE4d38e577B',
+        isDepositor: false,
+      },
+      cap: {
+        address: '0x16AD251B49E62995eC6f1b6A8F48A7004666397C',
+        underlying: null
+      },
+      strategies: [
+        {
+          name: 'Tarot Lender',
+          allocation: 1,
+          description: `
+          Supplies wFTM on Tarot decentralized lending protocol. 
+          Tarot gives users the opportunity to partecipate as lenders in isolated lending pools. 
+          
+          The strategy rebalances the available wFTM between Tarot isolated lending pools to have the best yield possible.          `,
+          links: [
+            {
+              name: 'Tarot',
+              to: 'https://www.tarot.to'
+            }
+          ]
+        }
+      ]
+    },
   ],
   selected: null,
   isLoading: false,
