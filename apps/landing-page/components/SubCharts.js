cf579e3b189d7c7b2d92382302511a784e941174
knx <gabrielebonumore@gmail.com>
2022-02-23 13:53:02 +0100

    update png to be lighter, remove nft from playbar, extend columns on subcharts

--- apps/landing-page/components/SubCharts.js
+++ apps/landing-page/components/SubCharts.js
@@ -88,7 +88,7 @@ const SubCharts = ({ underlyingData, marketCap, play, sentiment }) => {
   );
 
   return (
-    <section className="mb-10 gap-y-4 md:gap-4 grid auto-rows-fr grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    <section className="mb-10 gap-y-4 md:gap-4 grid auto-rows-fr grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <div className="flex flex-col">
         <Popover className="relative">
           <Popover.Button className="mb-2">
