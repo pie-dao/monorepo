0bf929412099f0b007f6d11a5764d16ec46ba023
knx <gabrielebonumore@gmail.com>
2022-04-27 14:32:11 +0200

    Update loader and twitter images

--- apps/landing-page/components/Loader.js
+++ apps/landing-page/components/Loader.js
@@ -1,8 +1,8 @@
 const Loader = () => {
   return (
-    <div className="flex flex-1 items-center justify-center text-highlight">
+    <div className="flex flex-1 items-center justify-center text-highlight my-4">
       <svg
-        className="animate-spin -ml-1 mr-3 h-5 w-5"
+        className="animate-spin h-5 w-5"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
