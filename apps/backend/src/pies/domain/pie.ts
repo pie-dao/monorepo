5431cedf6f345b722c1c44f4c8187397d0940cb5
adam-arold <arold.adam@gmail.com>
2022-04-30 13:58:18 +0200

    Separate Pie domain object from entity and add corresponding respository

---
+++ apps/backend/src/pies/domain/pie.ts
@@ -0,0 +1,28 @@
+import BigNumber from 'bignumber.js';
+
+export type Pie = {
+  name: string;
+  symbol: string;
+  address: string;
+  history: PieHistory[];
+  coingeckoId: string;
+};
+
+export type PieHistory = {
+  timestamp: string;
+  nav: number;
+  decimals: number;
+  marginalTVL: BigNumber;
+  totalSupply: BigNumber;
+  underlyingAssets: UnderlyingAsset[];
+};
+
+export type UnderlyingAsset = {
+  address: string;
+  amount: string;
+  usdPrice: string;
+  symbol?: string;
+  decimals?: number;
+  marginalTVL?: BigNumber;
+  marginalTVLPercentage?: number;
+};
