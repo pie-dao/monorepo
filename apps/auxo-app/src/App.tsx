2951784547385c402ca1f9ea4290e14c2e4e3cf0
Jordan <jordan.imran@predixa.ai>
2022-05-03 17:06:34 +0400

    removed homepage routes, add path aliases

--- apps/auxo-app/src/App.tsx
+++ apps/auxo-app/src/App.tsx
@@ -2,10 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { useSetWeb3Cache } from "./hooks/useCachedWeb3";
 import { useChainData } from "./hooks/multichain/useMultiChainData";
-import { Routes, Route } from "react-router-dom";
-import VaultDetails from "./pages/Vault";
 import AlertMessage from "./components/Header/AlertMessage";
-import Home from "./pages/Home";
 
 function App() {
   useSetWeb3Cache();
@@ -17,10 +14,6 @@ function App() {
           <Header />
           <section className="h-full w-full relative">
             <AlertMessage />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/vault/:vaultId" element={<VaultDetails />} />
-            </Routes>
           </section>
         </section>
         <Footer />
---
+++ apps/auxo-app/src/pages/vault/[vaultId].tsx
@@ -0,0 +1,102 @@
+import { useSelectedVault } from "@hooks/useSelectedVault";
+import DepositWithdrawSwitcher from "@components/Vault/Actions/DepositWithdrawSwitcher";
+import {
+  VaultExtendedInformationCard,
+  VaultStrategiesCard,
+} from "@components/Vault/Details/Cards";
+import VaultSummaryUser from "@components/Vault/Details/VaultSummaryUser";
+import {
+  VaultPoolAPY,
+  VEDoughStatusRow,
+  FloatingBackground,
+} from "@components/Vault/Details/StatusRows";
+import { useEffect } from "react";
+import { Vault } from "@store/vault/Vault";
+import { useAppDispatch } from "@hooks/index";
+import { setSelectedVault } from "@store/vault/vault.slice";
+import VaultCapSlider from "@components/Vault/Details/VaultCapSlider";
+import { useRouter } from "next/router";
+
+function VaultContentBlocks({
+  vault,
+}: {
+  vault: Vault | undefined;
+}): JSX.Element {
+  return (
+    <section className="grid grid-cols-1 gap-4">
+      <VaultExtendedInformationCard vault={vault} />
+      {vault && vault.strategies.length > 0 && (
+        <VaultStrategiesCard vault={vault} />
+      )}
+    </section>
+  );
+}
+
+function VaultActionBlocks({
+  vault,
+}: {
+  vault: Vault | undefined;
+}): JSX.Element {
+  return (
+    <section className="grid grid-cols-1 gap-4">
+      <div className="min-h-[20rem] bg-white border-gradient rounded-xl shadow-md">
+        <DepositWithdrawSwitcher vault={vault} />
+      </div>
+      <div className="bg-white rounded-xl shadow-md p-5">
+        <VaultSummaryUser vault={vault} loading={false} />
+      </div>
+    </section>
+  );
+}
+
+function VaultDetails(): JSX.Element {
+  const vault = useSelectedVault();
+  const {
+    query: { vaultId },
+  } = useRouter();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!vault && typeof vaultId === "string") {
+      dispatch(setSelectedVault(vaultId));
+    }
+  }, [vault, dispatch]);
+
+  return (
+    <section className="">
+      <VEDoughStatusRow />
+      <FloatingBackground />
+      <section
+        className="
+          grid
+          grid-cols-12
+          grid-flow-rows
+          gap-4
+          relative
+          mx-1 md:mx-5
+        "
+      >
+        <div
+          className="
+            col-span-12 lg:col-span-6 xl:col-span-6
+            order-2 lg:order-1 text-gray-700
+          "
+        >
+          <VaultPoolAPY vault={vault} />
+          <VaultContentBlocks vault={vault} />
+        </div>
+        <div
+          className="
+          col-span-12 lg:col-span-6 xl:col-span-6
+          order-1 lg:order-2 text-gray-700
+          "
+        >
+          <VaultCapSlider />
+          <VaultActionBlocks vault={vault} />
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default VaultDetails;
