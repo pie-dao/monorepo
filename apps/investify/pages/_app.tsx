74f7d2dcf86758609517e2c11c086d48723df106
knx <gabrielebonumore@gmail.com>
2022-05-13 12:38:24 +0200

    redux config

--- apps/investify/pages/_app.tsx
+++ apps/investify/pages/_app.tsx
@@ -1,17 +1,20 @@
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import Head from 'next/head';
+import { wrapper } from '../store';
 import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function CustomApp({ Component, ...rest }: AppProps) {
+  const { props, store } = wrapper.useWrappedStore(rest);
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Welcome to investify!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <Component {...props.PageProps} />
       </main>
-    </>
+    </Provider>
   );
 }
 
---
+++ apps/investify/store/index.ts
@@ -0,0 +1,42 @@
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+  AnyAction,
+} from '@reduxjs/toolkit';
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
+import vaultReducer from './vault/vault.slice';
+
+export const rootReducer = combineReducers({
+  vault: vaultReducer,
+});
+
+const reducer = (state: ReturnType<typeof rootReducer>, action: AnyAction) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state, // use previous state
+      ...action.payload, // apply delta from hydration
+    };
+    return nextState;
+  } else {
+    return rootReducer(state, action);
+  }
+};
+
+export const makeStore = () =>
+  configureStore({
+    reducer: reducer as never,
+  });
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppDispatch = ReturnType<typeof makeStore>['dispatch'];
+export type RootState = ReturnType<typeof makeStore>['getState'];
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export const wrapper = createWrapper<AppStore>(makeStore);
