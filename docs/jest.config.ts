245d7241bd0aa1a3ec55f35e60e146f2493752c8
Jordan <jordan.imran@predixa.ai>
2022-05-13 14:56:16 +0100

    moved docs to root and dropped as package

--- docs/jest.config.ts
+++ docs/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'sdk-utils-docs',
-  preset: '../../../jest.preset.ts',
+  preset: '../jest.preset.ts',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/sdk-utils/docs',
+  coverageDirectory: '../coverage/docs',
 };
