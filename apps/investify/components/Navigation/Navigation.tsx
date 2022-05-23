4eac9d99b3a4b3074495809b17eb71c8f86f00f4
knx <gabrielebonumore@gmail.com>
2022-05-23 12:32:50 +0200

    Sidebar animation improvement

--- apps/investify/components/Navigation/Navigation.tsx
+++ apps/investify/components/Navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction } from 'react';
 import { TemplateIcon, UsersIcon } from '@heroicons/react/solid';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
 import {
   AnimatePresence,
   motion,
@@ -10,44 +11,9 @@ import {
   Variants,
 } from 'framer-motion';
 import classNames from '../../utils/classnames';
+import useMediaQuery from '../../hooks/useMediaQuery';
 
-const navigation = [
-  { name: 'Dashboard', href: '/', icon: TemplateIcon },
-  { name: 'Discover', href: '/discover', icon: UsersIcon },
-];
-
-const listVariants: Variants = {
-  hidden: {},
-  visible: {
-    transition: {
-      delayChildren: 0.4,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const itemVariants: Variants = {
-  hidden: {
-    opacity: 0,
-  },
-  visible: {
-    opacity: 1,
-  },
-};
-
-const sidebarVariants = {
-  visible: {
-    width: 180,
-    x: 0,
-    opacity: 1,
-  },
-  hidden: {
-    width: 0,
-    x: -180,
-    opacity: 0,
-    transition: { delay: 0, duration: 0.2 },
-  },
-};
+type DragEvent = MouseEvent | TouchEvent | PointerEvent;
 
 export default function Navigation({
   open,
@@ -56,98 +22,193 @@ export default function Navigation({
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }) {
-  const { pathname } = useRouter();
+  const { t } = useTranslation();
+
+  const navigation = [
+    { name: t('Dashboard'), href: '/', icon: TemplateIcon },
+    { name: t('Discover'), href: '/discover', icon: UsersIcon },
+  ];
+
+  const isDesktop = useMediaQuery('(min-width: 640px)');
+
+  const sidebarVariants = !isDesktop
+    ? {
+        visible: {
+          width: 180,
+          x: 0,
+          opacity: 1,
+        },
+        hidden: {
+          width: 0,
+          x: -180,
+          opacity: 0,
+          transition: { delay: 0, duration: 0.2 },
+        },
+      }
+    : {
+        visible: {
+          width: 180,
+          x: 0,
+        },
+        hidden: {
+          width: 58,
+          x: 0,
+          transition: { delay: 0, duration: 0.2 },
+        },
+      };
+
+  const listVariants: Variants = !isDesktop
+    ? {
+        hidden: {},
+        visible: {
+          transition: {
+            delayChildren: 0.4,
+            staggerChildren: 0.1,
+          },
+        },
+      }
+    : {
+        hidden: {},
+        visible: {},
+      };
+
+  const itemVariants: Variants = !isDesktop
+    ? {
+        hidden: {
+          opacity: 0,
+        },
+        visible: {
+          opacity: 1,
+        },
+      }
+    : {
+        hidden: {},
+        visible: {},
+      };
+
+  const titleVariants: Variants = !isDesktop
+    ? {
+        hidden: {
+          opacity: 0,
+        },
+        visible: {
+          opacity: 1,
+        },
+      }
+    : {
+        hidden: {
+          opacity: 1,
+          width: 180,
+          x: 0,
+          transition: { delay: 0, duration: 0.2 },
+        },
+        visible: {
+          opacity: 1,
+          x: 15,
+          width: 40,
+          transition: { delay: 0, duration: 0.2 },
+        },
+      };
+
   const x = useMotionValue(0);
-  const handleDragEnd = (event: any, info: PanInfo) => {
+  const handleDragEnd = (_event: DragEvent, info: PanInfo) => {
     if (info.offset.x < -80) {
       setOpen(false);
     }
-    handleDrag(event, info);
+    handleDrag(_event, info);
   };
 
-  const handleDrag = (event: any, info: PanInfo) => {
+  const handleDrag = (_event: DragEvent, info: PanInfo) => {
     if (info.offset.x > 0) {
       x.set(0);
     }
   };
 
+  const { pathname } = useRouter();
+
   return (
     <AnimatePresence initial={false}>
-      {open && (
-        <motion.aside
-          animate={open ? 'visible' : 'hidden'}
-          initial="hidden"
-          exit="hidden"
-          drag="x"
-          variants={sidebarVariants}
-          dragConstraints={{
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-          }}
-          style={{ x }}
-          dragElastic={0.8}
-          dragMomentum={false}
-          onDrag={handleDrag}
-          onDragEnd={handleDragEnd}
-          className="h-full w-[180px]"
-        >
-          <motion.div className="flex flex-col flex-grow pt-5 h-full">
-            <div>
-              <div className="flex-shrink-0 flex items-center px-4 overflow-hidden">
-                <h2 className="text-2xl font-medium text-primary">Investify</h2>
-              </div>
-              <div className="mt-6 flex-1 overflow-y-auto">
-                <nav className="px-2 space-y-1 overflow-hidden font-medium">
-                  <motion.ul
-                    variants={listVariants}
-                    initial="hidden"
-                    animate="visible"
-                  >
-                    {navigation.map((item) => {
-                      const active = pathname === item.href;
-                      return (
-                        <motion.li
-                          key={item.name}
-                          variants={itemVariants}
-                          whileHover={
-                            !active && {
-                              scale: 1.03,
-                            }
+      <motion.aside
+        animate={open ? 'visible' : 'hidden'}
+        initial="hidden"
+        exit="hidden"
+        drag="x"
+        variants={sidebarVariants}
+        dragConstraints={{
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+        }}
+        style={{ x }}
+        dragElastic={0.8}
+        dragMomentum={false}
+        onDrag={handleDrag}
+        onDragEnd={handleDragEnd}
+        className="h-full w-[180px]"
+      >
+        <motion.div className="flex flex-col flex-grow pt-5 h-full">
+          <motion.div>
+            <motion.div className="flex-shrink-0 flex items-center px-4 overflow-hidden">
+              <motion.h2
+                className="text-2xl font-medium text-primary"
+                animate={open ? 'visible' : 'hidden'}
+                variants={titleVariants}
+              >
+                {open ? t('Investify') : 'In'}
+              </motion.h2>
+            </motion.div>
+            <div className="mt-6 flex-1 overflow-y-auto">
+              <nav className="px-2 space-y-1 overflow-hidden font-medium">
+                <motion.ul
+                  variants={listVariants}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  {navigation.map((item) => {
+                    const active = pathname === item.href;
+                    return (
+                      <motion.li
+                        key={item.name}
+                        variants={itemVariants}
+                        whileHover={
+                          !active && {
+                            scale: 1.03,
                           }
-                          whileTap={
-                            !active && {
-                              scale: 0.95,
-                            }
+                        }
+                        whileTap={
+                          !active && {
+                            scale: 0.95,
                           }
-                        >
-                          <Link href={item.href} passHref>
-                            <span
-                              className={classNames(
-                                active
-                                  ? 'text-primary cursor-default'
-                                  : 'text-gray-400 cursor-pointer hover:text-primary',
-                                'group flex items-center px-2 py-1 text-sm font-medium rounded-full border border-customBorder mb-2',
-                              )}
-                            >
-                              <item.icon
-                                className="mr-3 flex-shrink-0 h-6 w-6 text-primary"
-                                aria-hidden="true"
-                              />
+                        }
+                      >
+                        <Link href={item.href} passHref>
+                          <span
+                            className={classNames(
+                              active
+                                ? 'text-primary cursor-default'
+                                : 'text-gray-400 cursor-pointer hover:text-primary',
+                              'group flex items-center px-2 py-1 text-sm font-medium rounded-full border border-customBorder mb-2',
+                            )}
+                          >
+                            <item.icon
+                              className="flex-shrink-0 h-6 w-6 text-primary"
+                              aria-hidden="true"
+                            />
+                            <motion.span className="ml-4">
                               {item.name}
-                            </span>
-                          </Link>
-                        </motion.li>
-                      );
-                    })}
-                  </motion.ul>
-                </nav>
-              </div>
+                            </motion.span>
+                          </span>
+                        </Link>
+                      </motion.li>
+                    );
+                  })}
+                </motion.ul>
+              </nav>
             </div>
           </motion.div>
-        </motion.aside>
-      )}
+        </motion.div>
+      </motion.aside>
     </AnimatePresence>
   );
 }
