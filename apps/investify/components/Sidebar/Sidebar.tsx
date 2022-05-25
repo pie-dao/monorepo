8c4760ad676c02d0b43318fb0401c099d52633a8
knx <gabrielebonumore@gmail.com>
2022-05-25 17:53:23 +0200

    RHS sidebar layout update

--- apps/investify/components/Sidebar/Sidebar.tsx
+++ apps/investify/components/Sidebar/Sidebar.tsx
@@ -1,33 +1,9 @@
-import { AnimatePresence } from 'framer-motion';
-import useTranslation from 'next-translate/useTranslation';
-import SidebarBox from './SidebarBox';
-import { useAppSelector } from '../../hooks';
+import SidebarContent from './SidebarContent';
 
 export default function Sidebar() {
-  const { t } = useTranslation();
-  const { step } = useAppSelector((state) => state.sidebar);
-
   return (
     <aside className="h-screen hidden md:block md:w-2/5 max-w-sm px-4 py-5 pl-0 overflow-hidden relative">
-      <div className="h-full bg-white drop-shadow rounded-md p-4">
-        <h3 className="font-bold text-xl text-text capitalize w-full border-b-2 border-primary pb-2">
-          {t(step)}
-        </h3>
-        <AnimatePresence initial={false}>
-          <div className="flex">
-            {step === 'quote' && (
-              <SidebarBox key="quote" className="py-4">
-                <p className="text-text text-2xl">Tasty test</p>
-              </SidebarBox>
-            )}
-            {step === 'swap' && (
-              <SidebarBox key="swap" className="py-4">
-                <p className="text-text text-2xl">Very Tasty test</p>
-              </SidebarBox>
-            )}
-          </div>
-        </AnimatePresence>
-      </div>
+      <SidebarContent />
     </aside>
   );
 }
---
+++ apps/investify/components/Sidebar/SidebarContent.tsx
@@ -0,0 +1,30 @@
+import { AnimatePresence } from 'framer-motion';
+import SidebarBox from './SidebarBox';
+import useTranslation from 'next-translate/useTranslation';
+import { useAppSelector } from '../../hooks';
+
+export default function SidebarContent() {
+  const { t } = useTranslation();
+  const { step } = useAppSelector((state) => state.sidebar);
+  return (
+    <div className="h-full bg-white drop-shadow rounded-md p-4">
+      <h3 className="font-bold text-xl text-text capitalize w-full border-b-2 border-primary pb-2">
+        {t(step)}
+      </h3>
+      <AnimatePresence initial={false}>
+        <div className="flex">
+          {step === 'quote' && (
+            <SidebarBox key="quote" className="py-4">
+              <p className="text-text text-2xl">Tasty test</p>
+            </SidebarBox>
+          )}
+          {step === 'swap' && (
+            <SidebarBox key="swap" className="py-4">
+              <p className="text-text text-2xl">Very Tasty test</p>
+            </SidebarBox>
+          )}
+        </div>
+      </AnimatePresence>
+    </div>
+  );
+}
