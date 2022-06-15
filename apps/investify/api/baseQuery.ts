b9a5a81721a85693813e40854d38b40520d50b38
KnxPwr <gabrielebonumore@gmail.com>
2022-06-15 17:13:24 +0200

    fix(baseQuery): remove unused types

--- apps/investify/api/baseQuery.ts
+++ apps/investify/api/baseQuery.ts
@@ -2,11 +2,6 @@ import { BaseQueryFn } from '@reduxjs/toolkit/query/react';
 import { DocumentNode } from 'graphql';
 import { GraphQLClient, ClientError } from 'graphql-request';
 
-type P = Parameters<GraphQLClient['request']>;
-export type Document = P[0];
-export type Variables = P[1];
-export type RequestHeaders = P[2];
-
 export const graphqlRequestBaseQuery = (
   options:
     | { url: string; requestHeaders?: RequestHeaders }
