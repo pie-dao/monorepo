0756f25b5ddf9c5987fd835b11499335bfb23db3
KnxPwr <gabrielebonumore@gmail.com>
2022-06-21 11:12:07 +0200

    fix(vaults): add correct import types for vault to fix build

--- apps/investify/utils/networks.ts
+++ apps/investify/utils/networks.ts
@@ -3,3 +3,20 @@ export const SUPPORTED_CHAINS = {
   FANTOM: 250,
   POLYGON: 137,
 } as const;
+
+export type SUPPORTED_CHAIN_ID =
+  typeof SUPPORTED_CHAINS[keyof typeof SUPPORTED_CHAINS];
+
+export type NetworkDetail = {
+  chainId: string;
+  blockTime: number;
+  chainName: string;
+  nativeCurrency: {
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
+  rpcUrls: string[];
+  blockExplorerUrls: string[];
+};
+export type ChainMap = Record<SUPPORTED_CHAIN_ID, NetworkDetail>;
