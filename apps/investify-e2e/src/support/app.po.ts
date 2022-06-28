e739e205142ed34e2cdf6dbb413386878dd1e773
KnxPwr <gabrielebonumore@gmail.com>
2022-06-28 22:55:31 +0200

    feat(synpress): setup & login test

--- apps/investify-e2e/src/support/app.po.ts
+++ apps/investify-e2e/src/support/app.po.ts
@@ -1,3 +1,5 @@
 export const getGreeting = () => cy.get('h1');
-export const getAccountButton = () => cy.get('connect-button');
+export const getAccountButton = () => cy.get('#connect-button');
+export const getMetaMaskButton = () =>
+  cy.get('#piedao-metamask-connect-button');
 export const getButton = () => cy.get('button');
