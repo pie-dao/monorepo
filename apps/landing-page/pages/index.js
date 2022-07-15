e19ae280bce7aa01a5b31ea78c7316c7d82d8d7d
KnxPwr <gabrielebonumore@gmail.com>
2022-07-15 13:33:39 +0200

    fix(morePies): pause api call for more pies for the time being

--- apps/landing-page/pages/index.js
+++ apps/landing-page/pages/index.js
@@ -14,37 +14,37 @@ import ExploreProducts from '../components/ExploreProducts';
 import PlayBar from '../components/PlayBar';
 import Chart from '../components/Chart';
 import posts from '../content/twitterPosts.json';
+import morePies from '../content/morePies.json';
 
 export async function getServerSideProps({ req, res }) {
-  const expoloreMore = [
-    '0x8d1ce361eb68e9e05573443c407d4a3bed23b033',
-    '0xe4f726adc8e89c6a6017f01eada77865db22da14',
-  ];
+  // const expoloreMore = [
+  //   '0x8d1ce361eb68e9e05573443c407d4a3bed23b033',
+  //   '0xe4f726adc8e89c6a6017f01eada77865db22da14',
+  // ];
   const playAddress = '0x33e18a092a93ff21ad04746c7da12e35d34dc7c4';
   const play = await getCoinData(playAddress);
   // eslint-disable-next-line no-undef
-  const morePies = await Promise.all(
-    expoloreMore.map(async (pie) => {
-      const pieData = await getLatestHistory(pie);
-      return pieData;
-    }),
-  );
+  // const morePies = await Promise.all(
+  //   expoloreMore.map(async (pie) => {
+  //     const pieData = await getLatestHistory(pie);
+  //     return pieData;
+  //   }),
+  // );
 
   const cookies = new Cookies(req, res);
   const showCookiePolicy = cookies.get('cookiePolicy') !== 'accepted';
-  const sentiment = await getSentiment();
+  // const sentiment = await getSentiment();
 
   return {
     props: {
       play,
       showCookiePolicy,
-      morePies,
-      sentiment,
+      // sentiment,
     },
   };
 }
 
-export default function Home({ play, morePies, sentiment }) {
+export default function Home({ play, sentiment }) {
   return (
     <div className="text-white">
       <Hero actualPrice={play?.coin?.market_data?.current_price?.usd} />
