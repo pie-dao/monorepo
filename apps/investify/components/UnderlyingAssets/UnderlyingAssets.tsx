90858b2f7b3cb1bd02cb30ba2379f8f6413b26d3
KnxPwr <gabrielebonumore@gmail.com>
2022-07-22 10:53:24 +0200

    feat(UnderlyingAssets): Underlying assets accordion and layout

--- apps/investify/components/UnderlyingAssets/UnderlyingAssets.tsx
+++ apps/investify/components/UnderlyingAssets/UnderlyingAssets.tsx
@@ -1,9 +1,12 @@
+import { useMemo } from 'react';
 import Image from 'next/image';
+import { Disclosure } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import { AnimatePresence, motion } from 'framer-motion';
+import { orderBy } from 'lodash';
 import useTranslation from 'next-translate/useTranslation';
 import classNames from '../../utils/classnames';
-import { useMemo } from 'react';
 import { UnderlyingTokenEntity } from '../../api/generated/graphql';
-import { orderBy } from 'lodash';
 import {
   formatAsPercent,
   formatBalance,
@@ -55,116 +58,201 @@ export default function UnderlyingAssets({
             </div>
           </div>
         </div>
-        {sortedUnderlyingTokens.map((underlyingToken) => (
-          <div
-            key={underlyingToken.name}
-            className="bg-gradient-primary shadow-md rounded-lg px-3 py-2 overflow-hidden"
-            data-cy={`underlying-assets-${underlyingToken.name}`}
-          >
-            <div className="flex items-center">
-              <div className="min-w-0 flex-1 flex items-start">
-                <div className="flex-shrink-0 self-start">
-                  <Image
-                    src={`https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${underlyingToken.address}/logo.png`}
-                    alt={underlyingToken.name}
-                    height={40}
-                    width={40}
-                    className="rounded-full"
-                  />
-                </div>
-                <div className="min-w-0 flex-1 px-4 grid grid-cols-2 sm:grid-cols-5 sm:gap-4 items-center">
-                  <div className="flex flex-col justify-between">
-                    <p className="text-base font-bold" data-cy="name">
-                      {underlyingToken.symbol}
-                    </p>
-                    <p className="text-xs text-sub-dark">
-                      {underlyingToken.name}
-                    </p>
+        <div className="flex flex-col gap-5">
+          {sortedUnderlyingTokens.map((underlyingToken) => (
+            <Disclosure
+              key={underlyingToken.name}
+              as="div"
+              className="bg-gradient-primary shadow-md rounded-lg px-3 py-2 overflow-hidden"
+              data-cy={`underlying-assets-${underlyingToken.name}`}
+            >
+              {({ open }) => (
+                <>
+                  <Disclosure.Button
+                    as="div"
+                    className="cursor-pointer sm:cursor-auto sm:pointer-events-none"
+                  >
+                    <div className="flex items-center">
+                      <div className="min-w-0 flex-1 flex items-start">
+                        <div className="flex-shrink-0 self-center">
+                          <Image
+                            src={`https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${underlyingToken.address}/logo.png`}
+                            alt={underlyingToken.name}
+                            height={40}
+                            width={40}
+                            className="rounded-full"
+                          />
+                        </div>
+                        <div className="min-w-0 flex-1 pl-4 pr-0 sm:pr-4 grid grid-cols-2 sm:grid-cols-5 sm:gap-4 items-center">
+                          <div className="flex flex-col justify-between">
+                            <p className="text-base font-bold" data-cy="name">
+                              {underlyingToken.symbol}
+                            </p>
+                            <p className="text-xs text-sub-dark">
+                              {underlyingToken.name}
+                            </p>
+                          </div>
+                          <div className="flex sm:flex-col text-right sm:text-left items-center sm:items-start ml-auto sm:ml-0 gap-x-4">
+                            <div>
+                              <p className="text-base" data-cy="price">
+                                {formatBalanceCurrency(
+                                  underlyingToken.marketData[0].currentPrice,
+                                  defaultLocale,
+                                  defaultCurrency,
+                                )}
+                              </p>
+                              <p className="text-xs text-secondary">
+                                {`${formatBalanceCurrency(
+                                  underlyingToken.marketData[0]
+                                    .twentyFourHourChange.price,
+                                  defaultLocale,
+                                  defaultCurrency,
+                                )} / ${formatAsPercent(
+                                  underlyingToken.marketData[0]
+                                    .twentyFourHourChange.change,
+                                  defaultLocale,
+                                )}`}
+                              </p>
+                            </div>
+                            <motion.div
+                              initial={{ rotate: 0 }}
+                              animate={{ rotate: open ? 180 : 0 }}
+                              transition={{ duration: 0.2 }}
+                              className="flex sm:hidden"
+                            >
+                              <ChevronDownIcon
+                                className="h-5 w-5 text-primary"
+                                aria-hidden="true"
+                              />
+                            </motion.div>
+                          </div>
+                          <div className="flex-col justify-center text-right hidden sm:block">
+                            <p className={classNames(`text-base text-primary`)}>
+                              {formatBalance(
+                                underlyingToken.marketData[0].amountPerToken,
+                                defaultLocale,
+                                3,
+                              )}
+                            </p>
+                          </div>
+                          <div className="flex-col justify-center text-right hidden sm:block">
+                            <p
+                              className={classNames(
+                                `text-base text-primary font-medium`,
+                              )}
+                            >
+                              {formatBalance(
+                                underlyingToken.marketData[0].totalHeld,
+                                defaultLocale,
+                                3,
+                              )}
+                            </p>
+                          </div>
+                          <div className="hidden sm:block flex-col justify-center text-right">
+                            <p
+                              className={classNames(
+                                `text-base text-primary font-medium`,
+                              )}
+                            >
+                              {formatBalance(
+                                underlyingToken.marketData[0].allocation,
+                                defaultLocale,
+                                3,
+                              )}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Disclosure.Button>
+                  <AnimatePresence initial={false}>
+                    {open && (
+                      <Disclosure.Panel
+                        as={motion.div}
+                        initial="collapsed"
+                        animate="open"
+                        static
+                        exit="collapsed"
+                        key={'panel-' + underlyingToken.name}
+                        variants={{
+                          open: { opacity: 1, height: 'auto' },
+                          collapsed: { opacity: 0, height: 0 },
+                        }}
+                        transition={{
+                          duration: 0.2,
+                        }}
+                        className="bg-background rounded-lg overflow-hidden"
+                      >
+                        <motion.div
+                          variants={{
+                            collapsed: { scale: 0.8 },
+                            open: { scale: 1 },
+                          }}
+                          transition={{ duration: 0.3 }}
+                          className="origin-top-center p-4"
+                        >
+                          <div className="flex justify-between">
+                            <p>{t('amountPerToken')}</p>
+                            <p className="text-base text-primary">
+                              {formatBalance(
+                                underlyingToken.marketData[0].amountPerToken,
+                                defaultLocale,
+                                3,
+                              )}
+                            </p>
+                          </div>
+                          <div className="flex justify-between">
+                            <p>{t('totalHeld')}</p>
+                            <p className="text-base text-primary">
+                              {formatBalance(
+                                underlyingToken.marketData[0].totalHeld,
+                                defaultLocale,
+                                3,
+                              )}
+                            </p>
+                          </div>
+                          <div className="flex justify-between">
+                            <p>{t('allocation')}</p>
+                            <p className="text-base text-primary">
+                              {formatBalance(
+                                underlyingToken.marketData[0].allocation,
+                                defaultLocale,
+                                3,
+                              )}
+                            </p>
+                          </div>
+                        </motion.div>
+                      </Disclosure.Panel>
+                    )}
+                  </AnimatePresence>
+                  <div className="min-w-0 flex-1 flex gap-4 flex-col sm:flex-row">
+                    <div className="flex flex-1 items-center gap-x-4">
+                      <div className="flex flex-1 bg-gray-200 rounded-full h-1.5">
+                        <div
+                          className="bg-secondary h-1.5 rounded-full"
+                          style={{
+                            width: formatAsPercent(
+                              +underlyingToken.marketData[0].marginalTVLPercentage.toFixed(
+                                0,
+                              ),
+                            ),
+                          }}
+                        ></div>
+                      </div>
+                      <div className="text-sm" data-cy="percentage">
+                        {formatAsPercent(
+                          +underlyingToken.marketData[0].marginalTVLPercentage.toFixed(
+                            0,
+                          ),
+                        )}
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex-col justify-between text-right sm:text-left">
-                    <p className="text-base" data-cy="price">
-                      {formatBalanceCurrency(
-                        underlyingToken.marketData[0].currentPrice,
-                        defaultLocale,
-                        defaultCurrency,
-                      )}
-                    </p>
-                    <p className="text-xs text-secondary">
-                      {`${formatBalanceCurrency(
-                        underlyingToken.marketData[0].twentyFourHourChange
-                          .price,
-                        defaultLocale,
-                        defaultCurrency,
-                      )} / ${formatAsPercent(
-                        underlyingToken.marketData[0].twentyFourHourChange
-                          .change,
-                        defaultLocale,
-                      )}`}
-                    </p>
-                  </div>
-                  <div className="flex-col justify-center text-right hidden sm:block">
-                    <p className={classNames(`text-base text-primary`)}>
-                      {formatBalance(
-                        underlyingToken.marketData[0].amountPerToken,
-                        defaultLocale,
-                        3,
-                      )}
-                    </p>
-                  </div>
-                  <div className="flex-col justify-center text-right hidden sm:block">
-                    <p
-                      className={classNames(
-                        `text-base text-primary font-medium`,
-                      )}
-                    >
-                      {formatBalance(
-                        underlyingToken.marketData[0].totalHeld,
-                        defaultLocale,
-                        3,
-                      )}
-                    </p>
-                  </div>
-                  <div className="hidden sm:block flex-col justify-center text-right">
-                    <p
-                      className={classNames(
-                        `text-base text-primary font-medium`,
-                      )}
-                    >
-                      {formatBalance(
-                        underlyingToken.marketData[0].allocation,
-                        defaultLocale,
-                        3,
-                      )}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="min-w-0 flex-1 flex gap-4 flex-col sm:flex-row">
-              <div className="flex flex-1 items-center gap-x-4">
-                <div className="flex flex-1 bg-gray-200 rounded-full h-1.5">
-                  <div
-                    className="bg-secondary h-1.5 rounded-full"
-                    style={{
-                      width: formatAsPercent(
-                        +underlyingToken.marketData[0].marginalTVLPercentage.toFixed(
-                          0,
-                        ),
-                      ),
-                    }}
-                  ></div>
-                </div>
-                <div className="text-sm" data-cy="percentage">
-                  {formatAsPercent(
-                    +underlyingToken.marketData[0].marginalTVLPercentage.toFixed(
-                      0,
-                    ),
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+                </>
+              )}
+            </Disclosure>
+          ))}
+        </div>
       </div>
     </section>
   );
