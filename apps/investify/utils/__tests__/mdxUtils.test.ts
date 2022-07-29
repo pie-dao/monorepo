d7822d16747a457dba9da203faec4e3c69996475
KnxPwr <gabrielebonumore@gmail.com>
2022-07-29 18:19:28 +0200

    test(mdxUtils): write tests for mdx remote

--- apps/investify/utils/__tests__/mdxUtils.test.ts
+++ apps/investify/utils/__tests__/mdxUtils.test.ts
@@ -1,19 +1,40 @@
-import { getProductData } from '../mdxUtils';
+import { getProductData, getVaultData } from '../mdxUtils';
+import { suppressConsoleLogs } from '../testing/suppress-console-logs';
+
+const fallBackText = '### Content file missing or compiled with errors';
 
 describe('getProductData', () => {
   it('should display data for a given product', () => {
     const product = 'PLAY';
-    const productData = getProductData(product);
-    expect(productData.content.descriptionContent).toBeDefined();
-    expect(productData.content.thesisContent).toBeDefined();
-    expect(productData.content.investmentFocusContent).toBeDefined();
+    const { content } = getProductData(product);
+    expect(content.descriptionContent).toBeDefined();
+    expect(content.thesisContent).toBeDefined();
+    expect(content.investmentFocusContent).toBeDefined();
   });
 
-  it('should display fallback data for non existing products', () => {
-    const product = 'NONEXISTING';
-    const productData = getProductData(product);
-    expect(productData.content.descriptionContent).toBeDefined();
-    expect(productData.content.thesisContent).toBeDefined();
-    expect(productData.content.investmentFocusContent).toBeDefined();
+  it(
+    'should display fallback data for non existing products',
+    suppressConsoleLogs(() => {
+      const product = 'NONEXISTING';
+      const { content } = getProductData(product);
+      expect(content.descriptionContent).toMatch(fallBackText);
+      expect(content.thesisContent).toMatch(fallBackText);
+      expect(content.investmentFocusContent).toMatch(fallBackText);
+    }),
+  );
+
+  it('should display data for a given vault', () => {
+    const vault = '0xe2c30A7A0f9Ac17393203F1B4c0979Ab552fFe69';
+    const { content } = getVaultData(vault);
+    expect(content.aboutContent).toBeDefined();
   });
+
+  it(
+    'should display fallback data for non existing vaults',
+    suppressConsoleLogs(() => {
+      const vault = '0x0000000000000000000000000000000000000000';
+      const { content } = getVaultData(vault);
+      expect(content.aboutContent).toMatch(fallBackText);
+    }),
+  );
 });
