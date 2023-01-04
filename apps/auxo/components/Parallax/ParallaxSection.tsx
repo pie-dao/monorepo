3d7a83cbf421deb675e0127cbf05ec45ea9129d3
KnxPwr <gabrielebonumore@gmail.com>
2023-01-04 22:25:41 +0100

    testing parallax

---
+++ apps/auxo/components/Parallax/ParallaxSection.tsx
@@ -0,0 +1,95 @@
+import React, { useRef } from 'react';
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
+import Image from 'next/image';
+import AuxoLogotype from '../../public/images/home/hero/auxo-text.webp';
+import AuxoBg from '../../public/images/home/hero/auxo-bg-super.webp';
+import AuxoFluid from '../../public/images/home/hero/auxo-fluid-super.webp';
+import { motion } from 'framer-motion';
+import RiveComponent, { Fit, Layout, Alignment } from '@rive-app/react-canvas';
+
+const ParallaxSection: React.FC = () => {
+  const parallax = useRef<IParallax>(null);
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <Parallax ref={parallax} pages={2}>
+        <ParallaxLayer
+          offset={-0.3}
+          speed={1}
+          style={{ backgroundColor: '#fff' }}
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.2, duration: 0.8 }}
+        >
+          <ParallaxLayer offset={0} speed={0.3} factor={2}>
+            <Image
+              src={AuxoBg}
+              alt="AuxoDAO"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </ParallaxLayer>
+        </motion.div>
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.4, duration: 0.8 }}
+          >
+            <Image src={AuxoLogotype} alt="AuxoDAO" />
+          </motion.div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0} factor={2}>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.4, duration: 0.8 }}
+          >
+            <Image
+              src={AuxoFluid}
+              alt="AuxoDAO"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </motion.div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          className="flex place-items-center h-[350px]"
+          factor={1.5}
+          speed={5}
+        >
+          <RiveComponent
+            layout={
+              new Layout({
+                fit: Fit.ScaleDown,
+                alignment: Alignment.TopCenter,
+              })
+            }
+            src="/animations/hero-x.riv"
+          />
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+};
+export default ParallaxSection;
--- apps/auxo/pages/index.tsx
+++ apps/auxo/pages/index.tsx
@@ -1,46 +1,18 @@
 import { NavBar } from '../components/NavBar/NavBar';
 import { AuxoDAO, Lend, Support } from '../components/Home';
-import Parallax from '../components/Parallax/ParallaxImage';
-import Image from 'next/image';
-import head from '../public/images/home/hero/auxo-text.webp';
-import faded from '../public/images/home/hero/auxo-fluid.webp';
-import bg from '../public/images/home/hero/auxo-bg.webp';
+
+import ParallaxSection from '../components/Parallax/ParallaxSection';
 
 export default function Home() {
   return (
     <>
-      {/* <div className="relative h-0 pb-[65%] bg-[#00000c] overflow-hidden">
-        <div className="absolute inset-0">
-          <Parallax offset={0} className="z-10" speed={1}>
-            <Image
-              priority
-              src={head}
-              alt="hero image example"
-              className="w-full"
-            />
-          </Parallax>
-          <Parallax offset={0} className="z-0" speed={1.2}>
-            <Image
-              priority
-              src={bg}
-              alt="hero image example"
-              className="w-full"
-            />
-          </Parallax>
-          <Parallax offset={0} className="z-20 mt-36" speed={1.5}>
-            <Image
-              priority
-              src={faded}
-              alt="hero image example"
-              className="w-full"
-            />
-          </Parallax>
-        </div>
-      </div> */}
-      <NavBar />
-      <AuxoDAO />
-      <Lend />
-      <Support />
+      <ParallaxSection />
+      <div>
+        <NavBar />
+        <AuxoDAO />
+        <Lend />
+        <Support />
+      </div>
     </>
   );
 }
