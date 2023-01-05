b934014ba842050d39644798d56c21267e621a67
KnxPwr <gabrielebonumore@gmail.com>
2023-01-05 14:06:02 +0100

    testing parallax with vinz

--- apps/auxo/components/Parallax/ParallaxSection.tsx
+++ apps/auxo/components/Parallax/ParallaxSection.tsx
@@ -1,9 +1,14 @@
 import React, { useRef } from 'react';
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax';
 import Image from 'next/image';
+import grid from '../../public/images/home/hero/grid.jpg';
 import AuxoLogotype from '../../public/images/home/hero/auxo-text.webp';
 import AuxoBg from '../../public/images/home/hero/auxo-bg.webp';
 import AuxoFluid from '../../public/images/home/hero/auxo-fluid.webp';
+import uno from '../../public/images/home/hero/1.png';
+import dos from '../../public/images/home/hero/2.png';
+import tres from '../../public/images/home/hero/3.png';
+import quatro from '../../public/images/home/hero/4.png';
 import { motion } from 'framer-motion';
 import RiveComponent, { Fit, Layout, Alignment } from '@rive-app/react-canvas';
 
@@ -17,6 +22,34 @@ const ParallaxSection: React.FC = () => {
       }}
     >
       <Parallax ref={parallax} pages={2}>
+        <ParallaxLayer offset={0} speed={0.4} factor={2}>
+          <Image
+            src={tres}
+            alt="AuxoDAO"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="start"
+            className="mix-blend-color-dodge"
+          />
+          <Image
+            src={quatro}
+            alt="AuxoDAO"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="start"
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.6} factor={2}>
+          <Image
+            src={dos}
+            alt="AuxoDAO"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="start"
+          />
+        </ParallaxLayer>
+
+        {/*
         <ParallaxLayer
           offset={-0.3}
           speed={1}
@@ -72,11 +105,31 @@ const ParallaxSection: React.FC = () => {
           </motion.div>
         </ParallaxLayer>
 
+
         <ParallaxLayer
-          offset={1}
+          offset={1.8}
+          speed={1}
+          factor={2}
+          className="w-full grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center place-items-start"
+        >
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center">1</h1>
+            <p className="text-center">Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center">2</h1>
+            <p className="text-center">Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center">3</h1>
+            <p className="text-center">Lorem ipsum dolor sit amet.</p>
+          </div>
+        </ParallaxLayer> */}
+        <ParallaxLayer
+          offset={0.9}
           className="flex place-items-center h-[350px]"
-          factor={1.5}
-          speed={5}
+          factor={1}
+          speed={2}
         >
           <RiveComponent
             layout={
