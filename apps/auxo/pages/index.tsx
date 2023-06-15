1140c0d9da0521f590b1bd412fb94a968520df4e
KnxPwr <gabrielebonumore@gmail.com>
2023-06-15 15:06:19 +0200

    restore mainnet

--- apps/auxo/pages/index.tsx
+++ apps/auxo/pages/index.tsx
@@ -1,9 +1,10 @@
 import { NavBar } from '../components/NavBar/NavBar';
 import { AuxoDAO, Lend, Footer } from '../components/Home';
-
 import ParallaxSection from '../components/Parallax/ParallaxSection';
+import { useBodyClass } from '../hooks/useBodyClass';
 
 export default function Home() {
+  useBodyClass();
   return (
     <>
       <NavBar />
