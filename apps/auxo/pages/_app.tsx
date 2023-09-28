2f25c1fe35564e22a235f593addd6b68d0b03c70
KnxPwr <gabrielebonumore@gmail.com>
2023-09-28 16:24:24 +0200

    add error boundary

--- apps/auxo/pages/_app.tsx
+++ apps/auxo/pages/_app.tsx
@@ -21,6 +21,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import RewardsModalManager from '../components/Modals/Rewards/RewardsModalManager';
 import ClaimSuccess from '../components/Modals/Rewards/ClaimSuccess';
 import { MAINNET_RPC } from '../utils/networks';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 const wcV2InitOptions = {
   version: 2 as const,
@@ -97,28 +98,30 @@ function CustomApp({ Component, ...rest }: AppPropsWithLayout) {
   usePagesViews();
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <Web3ContextProvider>
-      <Web3OnboardProvider web3Onboard={web3Onboard}>
-        <Web3ReactProvider getLibrary={getLibrary}>
-          <QueryClientProvider client={queryClient}>
-            <Provider store={store}>
-              <Head>
-                <title>Welcome to AUXO</title>
-              </Head>
-              <GoogleAnalytics />
-              <div className="h-full">
-                <NotificationDisplay />
-                <ModalManager />
-                <ModalStakingSuccess />
-                <RewardsModalManager />
-                <ClaimSuccess />
-                {getLayout(<Component {...props.pageProps} />)}
-              </div>
-            </Provider>
-          </QueryClientProvider>
-        </Web3ReactProvider>
-      </Web3OnboardProvider>
-    </Web3ContextProvider>
+    <ErrorBoundary>
+      <Web3ContextProvider>
+        <Web3OnboardProvider web3Onboard={web3Onboard}>
+          <Web3ReactProvider getLibrary={getLibrary}>
+            <QueryClientProvider client={queryClient}>
+              <Provider store={store}>
+                <Head>
+                  <title>Welcome to AUXO</title>
+                </Head>
+                <GoogleAnalytics />
+                <div className="h-full">
+                  <NotificationDisplay />
+                  <ModalManager />
+                  <ModalStakingSuccess />
+                  <RewardsModalManager />
+                  <ClaimSuccess />
+                  {getLayout(<Component {...props.pageProps} />)}
+                </div>
+              </Provider>
+            </QueryClientProvider>
+          </Web3ReactProvider>
+        </Web3OnboardProvider>
+      </Web3ContextProvider>
+    </ErrorBoundary>
   );
 }
 
